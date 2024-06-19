6f56b3e5fa11cb8253f5dd1b7ef983e677689f12
changs0124 <haeya0124@gmail.com>
2024-06-19 11:10:57 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-function App() {
-  return (
-    <div>hello</div>
+/*
+  컴포넌트 : 함수 > HTML 태그를 리턴하는 함수
+          : 대문자.js로 생성
+          : 함수에 태그를 담아서 사용 > js export 후 사용하고 싶은 곳에서 import
+*/
 
-  );
+import { Fragment } from "react";
+import Hello from "./components/Hello";
+
+// App은 컴포넌트
+function App() {
+  // return <Fragment>
+  //           <div>
+  //             <Hello></Hello>
+  //           </div>
+  //           <div>
+  //             <Hello />
+  //           </div>
+  //       </Fragment>
+  // =
+  // Fragment 생략 가능
+  return <>
+            <div>
+              <Hello></Hello>
+            </div>
+            <div>
+              <Hello />
+            </div>
+        </>
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 해당 괄호안에 태그가 들어가면 작동함
+// 전역 라이브러리를 설정할 때 다른 때에는 App.js에서 처리
+root.render(<App />);
+// root.render(<div>test</div>);
+// root.render(<Hello />);
 
